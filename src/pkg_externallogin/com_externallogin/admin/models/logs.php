7518d7cbebb13261aaea79d086d61925bdbe2e8f
Jpeg08 <jp.bertrand@gmail.com>
2013-03-06 15:43:40 +0100

    Update logs.php

--- src/pkg_externallogin/com_externallogin/admin/models/logs.php
+++ src/pkg_externallogin/com_externallogin/admin/models/logs.php
@@ -137,7 +137,7 @@ class ExternalloginModelLogs extends JModelList
 		// Add the list ordering clause.
 		$orderCol = $this->state->get('list.ordering');
 		$orderDirn = $this->state->get('list.direction');
-		$query->order($db->getEscaped($orderCol.' '.$orderDirn));
+		$query->order($db->escape($orderCol.' '.$orderDirn));
 
 		return $query;
 	}
