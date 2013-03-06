090b3df678375d9d7d35bde583ecb2c7545d5962
Jpeg08 <jp.bertrand@gmail.com>
2013-03-06 15:47:06 +0100

    Update view.html.php

--- src/pkg_externallogin/com_externallogin/admin/views/about/view.html.php
+++ src/pkg_externallogin/com_externallogin/admin/views/about/view.html.php
@@ -25,7 +25,7 @@ jimport('joomla.application.component.view');
  *
  * @since  2.0.0
  */
-class ExternalloginViewAbout extends JView
+class ExternalloginViewAbout extends JViewLegacy
 {
 	/**
 	 * Execute and display a layout script.
